825ff3c68a19d40085b33083a1fe990c65af7a4b
Emmanuel <emmanuelamon92@gmail.com>
2021-05-06 08:54:28 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 // Write your solution in this file!
 const employee = {
-    name: 'John Doe',
-    streetAddress: '1234 John Island'
+ 
 };
 
 let updateEmployeeWithKeyAndValue = (employee, key, value) => {
-    const newObj = {...employee}
-    newObj['streetAddress'] = '11 Broadway'
-    return newObj
+
 }
 let destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
-    employee[key] = value;
-    return employee
+ 
 }
 let deleteFromEmployeeByKey = (employee, key) => {
-    const delObj = {...employee}
-    delete delObj[key]
-    return delObj
+
 }
 let destructivelyDeleteFromEmployeeByKey = (employee, key) => {
-    delete employee[key]
-    return employee
+
 }
